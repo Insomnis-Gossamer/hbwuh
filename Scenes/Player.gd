17b8939f821ba437827593a56aeabcb5c5d27555
Skyrunner23 <42549600+Skyrunner23@users.noreply.github.com>
2024-12-13 14:01:42 -0700

    Added geometry
    added some walls and pillars to jump on

--- Scenes/Player.gd
+++ Scenes/Player.gd
@@ -13,10 +13,12 @@ const SENSITIVITY = 0.003
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
+# Get the player's mouse input and move the camera
 func _unhandled_input(event):
 	if event is InputEventMouseMotion:
 		head.rotate_y(-event.relative.x * SENSITIVITY)
 		camera.rotate_x(-event.relative.y * SENSITIVITY)
+		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-90), deg_to_rad(90))
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
