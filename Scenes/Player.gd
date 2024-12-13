42d322ec3969afad78ff22a170069d54b05a4aea
Skyrunner23 <42549600+Skyrunner23@users.noreply.github.com>
2024-12-13 14:08:55 -0700

    Fixed movement
    WASD now moves you relative to the direction you're facing

--- Scenes/Player.gd
+++ Scenes/Player.gd
@@ -26,14 +26,14 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	if Input.is_action_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	# TODO: Add controller support and allow players to rebind buttons
 	var input_dir := Input.get_vector("left", "right", "up", "down")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var direction = (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
